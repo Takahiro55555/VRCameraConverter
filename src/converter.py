a1c3e8e3c08d4912e7d2898fcf79b5045bfd74fc
Takahiro55555 <tkhr.corei7@gmail.com>
2020-05-06 15:41:00 +0100

    Update: カメラ映像を複製し、横に連結した結果を表示できるようになった

--- src/converter.py
+++ src/converter.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 
 def capture_camera(mirror=True, size=None):
@@ -26,7 +27,7 @@ def capture_camera(mirror=True, size=None):
             frame = cv2.resize(frame, size)
 
         # フレームを表示する
-        cv2.imshow('camera capture', frame)
+        cv2.imshow('camera capture', convert_frame(frame))
 
         k = cv2.waitKey(1) # 1msec待つ
         if k == 27: # ESCキーで終了
@@ -36,5 +37,29 @@ def capture_camera(mirror=True, size=None):
     cap.release()
     cv2.destroyAllWindows()
 
+
+def convert_frame(frame, width=1334, height=750, draw_marker=False):
+    frame_height, frame_width, _ = frame.shape
+
+    magnification = height / frame_height  # 拡大率
+    resized_image_width = int(frame_width*magnification)
+    resized_image = cv2.resize(frame, (resized_image_width, height))
+
+
+    if draw_marker:
+        # 画像の中心にマーカーを描画する
+        cv2.drawMarker(resized_image, (int(resized_image_width/2)-25, int(height/2)-25), (0, 0, 255),
+                    markerType=cv2.MARKER_CROSS, markerSize=50, thickness=1, line_type=cv2.LINE_8)
+
+    trimed_image_width = int(width / 2)
+    trimed_image = resized_image[0:height, int(resized_image_width/2) - int(
+        trimed_image_width/2): int(resized_image_width/2) + int((trimed_image_width/2))]
+
+    # NOTE: 同じ画像を2枚横に並べているが、２眼カメラを導入した際はきちんと左右の画像をそれぞれ設定すること
+    converted_frame = np.hstack([trimed_image, trimed_image])  # 2つの配列（画像）を横に結合
+
+    return converted_frame
+
+
 if __name__ == "__main__":
-    capture_camera()
+    capture_camera(mirror=False)
